ffba9141e6306bb1b5ddee44ec7d81a58c1e8603
wandaoriana <wandalanatta5bod@gmail.com>
2024-08-25 09:30:27 -0300

    Agregando Ejercicio 02 de ciclos de java de la leccion 2

---
+++ Java/Leccion2/EjercicioCiclo02/src/Clase2/EjercicioCiclo02.java
@@ -0,0 +1,23 @@
+package EjercicioCiclos02;
+
+import javax.swing.JOptionPane;
+
+public class EjercicioCiclo02 {
+
+    public static void main(String[] args) {
+        int numero;
+
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero para verificar si es negativo o positivo. "));
+        while (numero != 0){
+            if(numero > 0){
+                JOptionPane.showMessageDialog(null, "El número ingresado ("+numero+") es positivo.");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "El número ingresado ("+numero+") es negativo");
+            }
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese otro numero para verificar si es positivo o negativo. "
+                    + "El programa finaliza cuando ingrese cero. "));
+        }
+        System.out.println("El programa se detuvo, se ingresaron numeros negativos o cero. ");
+    }
+}
