16f07ff77bcca0127c0ebf39a5d8844ace4b8b84
wandaoriana <wandalanatta5bod@gmail.com>
2024-09-06 08:26:16 -0300

    Agregando ejercicios numero 06 de ciclos de Java

--- Java/Leccion2/EjercicioCiclos06/src/Ciclos06/EjercicioCiclos06.java
+++ Java/Leccion2/EjercicioCiclos06/src/Ciclos06/EjercicioCiclos06.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class EjercicioCiclos06 {
+package Ciclos06;
+
+import javax.swing.JOptionPane;
+
+
+public class EjercicioCiclos06 {
+    public static void main(String[] args) {
+        int numero, suma = 0;
+        do{
+            numero = Integer.parseInt(JOptionPane.showInputDialog(" Digite un numero: "));
+            suma += numero;
+        }while (numero != 0);
+        JOptionPane.showMessageDialog(null, " La suma de todos los numeros ingresados es: " + suma);
+    }
 }
