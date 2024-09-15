13f53183f0e5a08f0b51ec6bef19c92f7f032de7
wandaoriana <wandalanatta5bod@gmail.com>
2024-09-15 08:12:25 -0300

    Agregando clase 04 de Java

---
+++ Java/Leccion2/aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,13 @@
+package Operaciones;
+
+public class Aritmetica {
+    // Atributos de la clase
+    int a;
+    int b;
+
+    // Método para sumar los números
+    public void sumarNumeros() {
+        int resultado = a + b;
+        System.out.println("La suma de " + a + " y " + b + " es: " + resultado);
+    }
+}
---
+++ Java/Leccion2/aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -0,0 +1,10 @@
+package Operaciones;
+
+public class PruebaAritmetica  {
+    public static void main(String[] args) {
+        Aritmetica aritmetica1 = new Aritmetica();
+        aritmetica1.a = 3;
+        aritmetica1.b = 7;
+        aritmetica1.sumarNumeros();
+    }
+}
