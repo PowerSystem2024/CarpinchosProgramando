7ae90dcff4ae17ae2d31e8d60f9a8fa9a09eeb4a
wandaoriana <wandalanatta5bod@gmail.com>
2024-09-15 05:59:00 -0300

    Agregando cambios de mi portafolio personal

--- Portafolio Personal/src/App.vue
+++ Portafolio Personal/src/App.vue
@@ -1,47 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import NavBar from './components/navBar.vue';
+import DatosPersonales from './components/DatosPersonales.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <!-- Barra de Navegación -->
+    <NavBar />
+    <!-- Componente Datos Personales -->
+    <DatosPersonales />
   </header>
-
   <main>
-    <TheWelcome />
+    <section id="educacion">
+      <h2>Educación - Cursos</h2>
+    </section>
+    <section id="experiencia">
+      <h2>Experiencia</h2>
+    </section>
+    <section id="proyectos">
+      <h2>Proyectos</h2>
+    </section>
+    <section id="habilidades">
+      <h2>Habilidades</h2>
+    </section>
+    <section id="intereses">
+      <h2>Intereses</h2>
+    </section>
   </main>
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ Portafolio Personal/src/components/DatosPersonales.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="datos-personales">
+      <h1>Wanda Oriana Lanatta</h1>
+      <h2>Programadora</h2>
+      <p>Hola, bienvenido a mi portafolio de proyectos</p>
+      <ul class="container-list">
+        <li><a href="#" class="icon-professional" width="35rem" alt="">LinkedIn</a></li>
+        <li><a href="#" class="icon-professional" width="35rem" alt="">GitHub</a></li>
+        <li><a href="#" class="icon-professional" width="35rem" alt="">GitLab</a></li>
+      </ul>
+      <h3>Ciudad de Mendoza, Argentina</h3>
+    </section>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  </style>
+  
