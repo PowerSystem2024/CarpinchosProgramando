b14875fcf1aea9d9839c762dc5706bee0f261427
Mariana Aguilera <marian5174@gmail.com>
2024-10-01 15:33:45 -0300

    Subo repositorio de Mariana

---
+++ TECNICATURA/PORTAFOLIO/INDIVIDUAL/MarianaAguilera/Portafolio.Vue.js/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import NavBar from './components/NavBar.vue';
+import DatosPersonales from './components/DatosPersonales.vue';
+import Educacion from './components/EducacionComponente.vue';
+import ExperienciaComponente from './components/ExperienciaComponente.vue';
+</script>
+
+<template>
+   <header> <!--barra de navegación-->
+    <NavBar/>
+    <DatosPersonales/>
+   </header> 
+   <main>
+        <section id="educacion"><h2>Educación - Cursos</h2>
+          <Educacion/>
+        </section>
+        <section id="experiencia"><h2>Experiencia</h2></section>
+          <ExperienciaComponente/>
+        <section id="proyectos"><h2>Proyectos</h2></section>
+        <section id="habilidades"><h2>Habilidades</h2></section>
+        <section id="intereses"><h2>Intereses</h2></section>
+   </main>
+</template>
+
+<style scoped>
+
+
+</style>
