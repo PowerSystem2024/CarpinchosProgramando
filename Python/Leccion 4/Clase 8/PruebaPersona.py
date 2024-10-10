88e361ab4dc784cb7ce3ef1a604e85928587257f
wandaoriana <wandalanatta5bod@gmail.com>
2024-10-10 01:09:17 -0300

    Agregando Clase 08 de Python

---
+++ Python/Leccion 4/Clase 8/PruebaPersona.py
@@ -0,0 +1,13 @@
+from Persona2 import Persona2
+
+print("Creacion de objetos".center(50,"-"))
+
+if __name__ == "__main__":
+    persona5 =  Persona2("Gonzalo","Cambero",29)
+    persona5.mostrarDetalle()
+
+    print(__name__)
+
+print("Eliminacion de objetos".center(30,"-"))
+
+del persona5
