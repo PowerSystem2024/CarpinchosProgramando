b9cd1deda20105215f79b9d2f1a03b850741b2f5
wandaoriana <wandalanatta5bod@gmail.com>
2024-10-10 01:03:38 -0300

    Modificando Carpetas

--- JavaScript/Tecnicatura3JS/TSleccion01/Leccion07/07-01.js
+++ JavaScript/Tecnicatura3JS/TSleccion01/Leccion07/07-01.js
@@ -1,4 +1,8 @@
+// NO SE PUEDE PORQ ANTES HAY Q CREAR LA CLASE
+// let perosna3 = new Persona("Carlos","Joyst",66)
+
 class Persona { // CLASE PADRE
+    static contadorObjetosPersona= 6;
     constructor(nombre,apellido,edad) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -19,7 +23,25 @@ class Persona { // CLASE PADRE
     set apellido(apellido) {
         this._apellido = apellido;
     }
+
+    nombreCompleto() {
+        return this.nombre + " " + this.apellido
+    }
+    toString() {
+        return this.nombreCompleto();
+    }
+
+    static saludar() {
+        console.log('Saludos desde este método static');
+    }
+
+    static saludar2(persona) {
+    console.log(persona.nombre);
 }
+
+}
+
+
 
 class Empleado extends Persona { // CLASE HIJA, HEREDA DE PERSONA 
     constructor(nombre,apellido,edad,departamento) {
@@ -29,26 +51,37 @@ class Empleado extends Persona { // CLASE HIJA, HEREDA DE PERSONA
     get departamento() {
         return this._departamento
     }
-    set departamento(departamento) {
+    set departamento(departamento) { 
         this._departamento = departamento;
+    }
+
+    // Sobreescribo el metodo de la clase Padre (Persona)
+    nombreCompleto() {
+        return super.nombreCompleto() + ", " + this._departamento
     }
 }
 
 // Crear el método get y set para el atributo de apellido, luego modificar el apellido a través del  set y 
 // mostrar con un console.log lo que es el get donde muestra el resultado obtenido.
 
-let persona1 = new Persona("Wanda", "Lanatta", 24);
-let persona2 = new Persona("Samira", "Lopez", 40);
+let persona1 = new Persona("Wanda", "Lanatta", 28);
+let persona2 = new Persona("Carlos", "Lanatta", 30);
 
 persona1.apellido = "CAMBIOAPELLIDO"
 console.log(persona1.apellido);
 
-let empleado1 = new Empleado("Fernando","Simich",38,"1C");
-console.log(empleado1)
-let empleado1 = new Empleado("Jose","Perez",35,"1C");
-
+let empleado1 = new Empleado("Mercedes","Perez",35,"Programacion");
 console.log(empleado1.nombreCompleto())
 
 // Object.prototype.toString Manera de acceder a atributos y metodos de forma dinamica
 console.log(empleado1.toString())
 console.log(persona1.toString())
+
+//persona1.saludar(); no se utiliza desde el objeto
+Persona.saludar();
+Persona.saludar2(Persona1);
+
+Empleado.saludar2(empleado1);
+//console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);  // 5
+console.log(Empleado.contadorObjetosPersona);  // 5
---
+++ PortafolioPersonal/src/components/ExperienciaComponente.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue';
+const titulo = 'Desarrollador Full Stack';
+const fecha = 'Desde Marzo 2024';
+const experiencias = ref([
+    {id: 1, src:'/src/assets/ventas.svg', parrafo: 'Lidere un equipo de 5 desarrolladores'}
+])
+</script>
+
+<template>
+    <div class="card">
+        <h3 class="titulo">{{titulo}}</h3>
+        <p class="fecha">{{fecha}}</p>
+        <ul class="listado">
+            <li class="item" v-for="experiencia in experiencias" :key="experiencia.id">
+                <img class="image-svg" :src="experiencia.src" width="45rem" :alt="experiencia.parrafo">
+                <p>{{ experiencia.parrafo }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.card{
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    background-color:#bdd6d2;
+    border-radius: 15px;
+}
+
+.titulo{
+    font-size: 1.5rem;
+    color: #586875;
+}
+
+.fecha{
+    font-size: 1rem;
+    color:#586875;;
+    margin-bottom: 1rem;
+}
+
+.listado{
+    display: flex;
+    flex-direction: column;
+}
+
+.item{
+    align-items: center;
+    display: flex;
+    padding: 1rem;
+    gap: 1.5rem;
+}
+</style>
