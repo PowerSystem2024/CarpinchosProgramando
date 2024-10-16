c3478dfb2a1ddceb0b9ae58743540323d79bb07e
wandaoriana <wandalanatta5bod@gmail.com>
2024-10-16 15:50:35 -0300

    Agregando Clase 8 de python

---
+++ Java/Leccion5/Persona/src/Test/TestHerencia.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado();
+        System.out.println("empleado1" ="" + empleado1);
+
+    }
+}
--- Python/Leccion 4/Clase 8/PruebaPersona.py
+++ Python/Leccion 4/Clase 8/PruebaPersona.py
@@ -3,7 +3,7 @@ from Persona2 import Persona2
 print("Creacion de objetos".center(50,"-"))
 
 if __name__ == "__main__":
-    persona5 =  Persona2("Gonzalo","Cambero",29)
+    persona5 =  Persona2("Fernando","Simich",29)
     persona5.mostrarDetalle()
 
     print(__name__)
