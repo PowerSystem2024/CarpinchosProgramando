b7c12752f5c0e65dbd8726539da5218d98ccb3b7
anitaprg <167032821+anaprg05@users.noreply.github.com>
2024-10-22 22:00:54 -0300

    Elimino carpeta extra

--- TECNICATURA/PORTAFOLIO/INDIVIDUAL/AnaPaulaRiosGarin/Portafolio Ana/Portafolio VueJS/src/main.js
+++
@@ -1,8 +0,0 @@
-// main.js - Es el punto de entrada de la aplicación
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
-    
