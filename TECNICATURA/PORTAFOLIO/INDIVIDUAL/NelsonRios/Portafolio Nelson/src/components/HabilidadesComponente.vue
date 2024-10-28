e4f442c0acafd6582d66108319e1c3c48cdb674f
Nelrios <nelsonriosapp@gmail.com>
2024-10-28 02:35:18 -0300

    Agrego portafolio personal avazado

---
+++ TECNICATURA/PORTAFOLIO/INDIVIDUAL/NelsonRios/Portafolio Nelson/src/components/HabilidadesComponente.vue
@@ -0,0 +1,148 @@
+<script setup>
+import img1 from '../components/icons/js.svg';
+import img2 from '../components/icons/python.svg';
+import img3 from '../components/icons/html-css.svg';
+import img4 from '../components/icons/java.svg';
+import img5 from '../components/icons/sql.svg';
+import img7 from '../components/icons/vue.svg';
+import img8 from '../components/icons/node.svg';
+import img10 from '../components/icons/bootstrap.svg';
+import img11 from '../components/icons/git.svg';
+import img15 from '../components/icons/visual.svg';
+import img17 from '../components/icons/mysql.svg';
+import img19 from '../components/icons/arg.svg';
+import img20 from '../components/icons/eeuu.svg';
+
+//Se importa ref de vue para poder usarlo
+import { ref } from 'vue';
+//Se crea un array de objetos con las habilidades
+//Este es un array de objetos con habilidades que se pueden modificar o agregar
+const habilidades = ref([
+    {// Se crea un objeto con id, nombre y habilidades
+        id: 1, nombre: 'Lenguajes de Programación', habilidades: [
+            //Se crea un objeto con id, nombre, nivel e icono
+            { id: 1, nombre: 'JavaScript', nivel: 'Intermedio', icono: img1 },
+            { id: 2, nombre: 'Python', nivel: 'Intermedio', icono: img2 },
+            { id: 3, nombre: 'HTML/CSS', nivel: 'Intermedio', icono: img3 },
+            { id: 4, nombre: 'Java', nivel: 'Intermedio', icono: img4 },
+            { id: 5, nombre: 'SQL', nivel: 'Intermedio', icono: img5 }
+        ]
+    },
+    {
+        id: 2, nombre: 'Frameworks y Librerías', habilidades: [
+            { id: 2, nombre: 'Vue.js', nivel: 'Intermedio', icono: img7 },
+            { id: 3, nombre: 'Node.js', nivel: 'Intermedio', icono: img8 },
+            { id: 5, nombre: 'Bootstrap', nivel: 'Intermedio', icono: img10 }
+        ]
+    },
+    {
+        id: 3, nombre: 'Herramientas y Software', habilidades: [
+            { id: 1, nombre: 'Git', nivel: 'Intermedio', icono: img11 },
+            { id: 5, nombre: 'Visual Studio', nivel: 'Intermedio', icono: img15 }
+        ]
+    },
+    {
+        id: 4, nombre: 'Bases de Datos', habilidades: [
+            { id: 2, nombre: 'MySQL', nivel: 'Intermedio', icono: img17 },
+
+        ]
+    },
+    {
+        id: 5, nombre: 'Idiomas', habilidades: [
+            { id: 1, nombre: 'Español', nivel: 'Nativo', icono: img19 },
+            { id: 2, nombre: 'Inglés', nivel: 'Basico', icono: img20 }
+        ]
+    }
+]);
+
+</script>
+
+
+<template>
+    <!--Este componente vue funciona de esta forma:-->
+    <div class="skills-contenedor">
+        <!--Se recorre el array habilidades con v-for y se asigna la key habilidad.id-->
+        <div v-for="habilidad in habilidades" :key="habilidad.id" class="skills-categoria">
+            <!--Se muestra el nombre de la habilidad-->
+            <h3>{{ habilidad.nombre }}</h3>
+            <ul class="skills">
+                <!--Se recorre el array habilidades con v-for y se asigna la key habilidad.id-->
+                <li v-for="habilidad in habilidad.habilidades" :key="habilidad.id" class="skill">
+                    <!--Se muestra el icono de la habilidad y el nombre de la habilidad con su nivel-->
+                    <img :src="habilidad.icono" :alt="habilidad.nombre">
+                    <!--Se muestra el icono de la habilidad y el nombre de la habilidad con su nivel-->
+                    <span>{{ habilidad.nombre }}: {{ habilidad.nivel }}</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.skills-contenedor {
+    padding: 2rem;
+    background: linear-gradient(-45deg, #021526, #03346E, #6EACDA, #E2E2B6);
+}
+
+.skills-categoria {
+    margin-bottom: 20px;
+}
+
+.skills-categoria h3 {
+    display: flex;
+    justify-content: left;
+    margin-bottom: 10px;
+    font-size: 1.5em;
+    color: aliceblue;
+    font-weight: bold;
+}
+
+.skills {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    gap: 20px;
+}
+
+.skill {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    background-color: rgba(241, 245, 243, 0.856);
+    padding: 0.3em;
+    border-radius: 8px;
+    box-shadow: 0px 4px 12px rgba(2, 151, 151, 0.963);
+    flex: 1 1 200px;
+    max-width: 250px;
+}
+
+.skill:hover {
+    background-color: rgba(2, 151, 151, 0.963);
+}
+
+.skill img {
+    width: 35px;
+    height: 35px;
+}
+
+.skill span {
+    font-size: 1em;
+    color: #333;
+}
+
+.skill:hover {
+    font-size: 1.3em;
+    transition: 2s ease;
+}
+
+@media (max-width: 768px) {
+    .skills-categoria h3 {
+        justify-content: center;
+    }
+
+    .skill {
+        justify-content: center;
+    }
+}
+</style>
---
+++ TECNICATURA/PORTAFOLIO/INDIVIDUAL/NelsonRios/Portafolio Nelson/src/components/InteresesComponente.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue';
+import FondoLava from './FondoLava.vue';
+//Este es un arreglo con ref para que se pueda reactivar el cambio de los intereses
+const intereses = ref([
+    'Exploración en Programación: Estoy comenzando en el mundo de la programación y aprendiendo varios lenguajes. Aunque soy nuevo en esta área, disfruto mucho cada paso del aprendizaje y me motiva la idea de mejorar continuamente.',
+    'Creatividad e Innovación: Soy una persona curiosa y meticulosa, con experiencia como matricero industrial, lo que me ha enseñado a prestar atención al detalle. Me apasiona investigar sobre nuevas tecnologías y disfruto aplicando mi creatividad a cada proyecto en el que trabajo.'
+]);
+</script>
+
+<template>
+    <div class="intereses-contenedor">
+        <FondoLava />
+        <ul class="contenedor-lista">
+            <li class="item" v-for="interes in intereses" :key="interes">
+                {{ interes }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.intereses-contenedor {
+    display: flex;
+    position: relative;
+    margin: 0 auto;
+    max-width: 100%;
+    padding: 1rem;
+}
+
+.contenedor-lista {
+    list-style-type: none;
+    padding: 1rem;
+    margin: 0;
+    width: 100%;
+    height: 100%;
+}
+
+.item {
+    margin-bottom: 1rem;
+    padding: 1rem; /* Añade espacio alrededor del texto */
+    background-color: rgba(9, 9, 9, 0.5); /* Fondo negro semitransparente */
+    border-radius: 8px; /* Bordes redondeados */
+    color: white; /* Color del texto */
+    font-size: 25px; /* Tamaño de fuente */
+    text-shadow: 3px 3px 6px rgb(88, 201, 232); /* Sombra del texto */
+}
+</style>
